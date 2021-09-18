0f40d4896c5bcc2d6dff15c51f11c8ecc48beb34
Manuel Setyo Saputro Sriwibowo <111201811122@mhs.dinus.ac.id>
2021-09-18 13:57:10 +0700

    latihan 2

---
+++ Latihan2_2D/Assets/Script/Coba.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coba : MonoBehaviour
+{
+    public string myName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("I am live and my name is " + myName);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
